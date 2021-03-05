e084f583a201bf29eaad3e8770fc314ccc6444a3
geff <geoffwburman@gmail.com>
2021-03-05 12:14:35 +0800

    added random object spawner (used for tiles currently) added dummy tile prefabs added first room prefab (LR)

---
+++ LevelGenerator/Assets/Scripts/RandomObjectSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] objects;
+
+    private void Start()
+    {
+        int rand = Random.Range(0, objects.Length);
+
+        Instantiate(objects[rand], transform.position, Quaternion.identity);
+    }
+}
