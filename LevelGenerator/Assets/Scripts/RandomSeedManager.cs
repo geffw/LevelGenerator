8d6e3d3e3ee3e5a78b6131e132e65abc45db69c8
geff <geoffwburman@gmail.com>
2021-03-05 12:04:03 +0800

    added random seed manager

---
+++ LevelGenerator/Assets/Scripts/RandomSeedManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[DefaultExecutionOrder(-10000)]
+public class RandomSeedManager : MonoBehaviour
+{
+    [SerializeField] private bool useSeed;
+    [SerializeField] private int seed;
+    
+    private void Awake()
+    {
+        if (!useSeed) return;
+        
+        Random.InitState(seed);
+
+        Debug.Log($"Random seed value set ({seed}). Test random range: {Random.Range(0, 1000000)}");
+    }
+}
