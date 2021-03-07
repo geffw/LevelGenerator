7113d1b524043e0436f271d078416a0cdf651350
geff <geoffwburman@gmail.com>
2021-03-07 09:35:50 +0800

    added room spawning (currently not checking for re-moves, borders, etc) fixed new gameobject duplication

--- LevelGenerator/Assets/Scripts/LevelGenerator.cs
+++ LevelGenerator/Assets/Scripts/LevelGenerator.cs
@@ -10,22 +10,77 @@ public class LevelGenerator : MonoBehaviour
     
     private List<GameObject> roomList = new List<GameObject>();
     private GameObject rooms;
+    
+    private int direction;
+    private float roomSpawnRate = 0.25f;
+    private float roomSpawnTimer;
 
     private void Start()
     {
         SpawnOuterBorders();
 
-        rooms = Instantiate(new GameObject(), transform);
+        rooms = new GameObject();
         rooms.transform.name = "Rooms";
 
         int randStartPos = Random.Range(0, gridSize);
+        transform.position = new Vector3(transform.position.x + randStartPos * 10f + 5f, 5f, 0f);
+    }
 
-        SpawnFirstRoom(randStartPos);
+    private void Update()
+    {
+        if (roomSpawnTimer <= 0f)
+        {
+            Move();
+            roomSpawnTimer = roomSpawnRate;
+        }
+        else
+        {
+            roomSpawnTimer -= Time.deltaTime;
+        }
+    }
+
+    private void Move()
+    {
+        direction = Random.Range(1, 6);
+        
+        switch (direction)
+        {
+            case 1:
+            case 2:
+            {
+                Vector2 newPos = new Vector2(transform.position.x + 10f, transform.position.y);
+                transform.position = newPos;
+                break;
+            }
+            case 3:
+            case 4:
+            {
+                Vector2 newPos = new Vector2(transform.position.x - 10f, transform.position.y);
+                transform.position = newPos;
+                break;
+            }
+            case 5:
+            {
+                Vector2 newPos = new Vector2(transform.position.x, transform.position.y - 10f);
+                transform.position = newPos;
+                break;
+            }
+        }
+        
+        SpawnRoom();
+    }
+
+    private void SpawnRoom()
+    {
+        GameObject room = Instantiate(roomPrefabs[0], transform.position, Quaternion.identity);
+        roomList.Add(room);
+        room.transform.parent = rooms.transform;
+        room.transform.name = $"Room{roomList.IndexOf(room)}";
     }
 
     private void SpawnOuterBorders()
     {
-        GameObject outerBorders = Instantiate(new GameObject(), transform);
+        GameObject outerBorders = new GameObject();
         outerBorders.transform.name = "OuterBorders";
 
         Vector3 outerBorderBottomPos = new Vector3(transform.position.x + gridSize * (10f / 2f) + 0.5f,
@@ -53,13 +108,5 @@ public class LevelGenerator : MonoBehaviour
             Instantiate(outerBorderPrefab, outerBorderLeftPos, Quaternion.Euler(new Vector3(0f, 0f, 90f)));
         outerBorderLeft.transform.localScale = new Vector3(gridSize * 10f + 1f, 1, 1);
         outerBorderLeft.transform.parent = outerBorders.transform;
-    }
-
-    private void SpawnFirstRoom(int pos)
-    {
-        GameObject room = Instantiate(roomPrefabs[0], new Vector3(transform.position.x + pos * 10f + 5f, 5f, 0f), Quaternion.identity);
-        roomList.Add(room);
-        room.transform.parent = rooms.transform;
-        room.transform.name = $"Room{roomList.IndexOf(room)}";
     }
 }
